2ce8e6b30985475e6912a6825191c7af9b956290
omkar <omkar.c.344@gmail.com>
2022-05-09 15:55:58 +0530

    added test cases

---
+++ tests/tokenTest.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+
+describe("Token Contract", function () {
+  let Token;
+  let MyToken;
+  let owner;
+  let initialSupply = BigNumber.from(1000000000).mul(BigNumber.from(10).pow(9));
+
+  beforeEach(async function () {
+    Token = await ethers.getContractFactory("MyToken");
+    [owner] = await ethers.getSigners();
+    MyToken = await Token.deploy(initialSupply);
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await MyToken.owner()).to.equal(owner.address);
+    });
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await MyToken.balanceOf(owner.address);
+      expect(await MyToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+});
+ 
