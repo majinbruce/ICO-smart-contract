f39a34eaf5e41d67585e13d513747cb637dfb01a
omkar <omkar.c.344@gmail.com>
2022-05-09 15:57:19 +0530

    added ownable functionality

--- contracts/token.sol
+++ contracts/token.sol
@@ -2,10 +2,11 @@
 pragma solidity >=0.8.1;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20,Ownable {
     constructor(uint256 initialSupply) ERC20("MyToken", "MTN") {
-        _mint(msg.sender, initialSupply);
+        _mint(owner(), initialSupply);
     }
 
     function decimals() public view virtual override returns (uint8) {
