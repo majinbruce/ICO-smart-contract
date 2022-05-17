568055112a4ab6fc7cf2560d1ddf12ea2cd175ce
omkar <omkar.c.344@gmail.com>
2022-05-17 13:42:15 +0530

    removed enums and added uint for stages

--- contracts/ico.sol
+++ contracts/ico.sol
@@ -2,16 +2,14 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract MyTokenCrowdsale is Crowdsale {
     constructor(
         uint256 rate,
         address payable wallet,
         IERC20 token
-    ) public Crowdsale(rate, wallet, token) {
-        //setting the initial values of mappings from constructor arg values
-        stageTokens[crowdSaleStage.preSale] = preSaleQty;
-    }
+    ) public Crowdsale(rate, wallet, token) {}
 
     uint256 preSaleQty = 30000000 * 10**9;
     uint256 seedSaleQty = 50000000 * 10**9;
@@ -19,53 +17,41 @@ contract MyTokenCrowdsale is Crowdsale {
     uint256 preSalePrice = 22666;
     uint256 seedSalePrice = 13600;
     uint256 publicSalePrice = 3400;
+
     // crowdsale stages
-
-    enum crowdSaleStage {
-        preSale,
-        seedSale,
-        publicSale
-    }
-
-    // set default stage to pre sale stage
-    crowdSaleStage constant defaultStage = crowdSaleStage.preSale;
-    crowdSaleStage stage;
+    uint256 preSale = 0;
+    uint256 seedSale = 1;
+    uint256 publicSale = 2;
 
     //this mapping tracs the amount of tokens available per stage
-    mapping(bytes32 => uint256) private stageTokens;
+    mapping(uint256 => uint256) private stageTokens;
 
-    function getPresale() private view returns (uint256) {
-        return stageTokens[sha3(crowdSaleStage.preSale)];
-    }
-
-    function setpreSale(uint256 qty) private view {
-        stageTokens[getPresale()] = qty;
+    function setStageTokenMapping() private {
+        stageTokens[preSale] = preSaleQty;
+        stageTokens[seedSale] = seedSaleQty;
+        stageTokens[publicSale] = publicSaleQty;
     }
 
     //this mapping tracs the rate per stage
-    mapping(bytes32 => uint256) private stageRate;
-
-    function getCrowdSaleStage() public view returns (crowdSaleStage) {
-        return stage;
-    }
+    mapping(uint256 => uint256) private stageRate;
 
     // function getRate() private view returns () {}
 
-    function _getTokenAmount(uint256 weiAmount, rate)
+    function _getTokenAmount(uint256 weiAmount, uint256 rate)
         internal
         view
         returns (uint256)
     {
-        if (stage = crowdSaleStage.preSale) {
+        {
             return (weiAmount.mul(rate));
         }
     }
-
-    function currentSaleStage() public view returns (stage _stage) {
+    /*
+    function currentSaleStage() public view returns (crowdSaleStage _stage) {
         return saleStage;
     }
 
-    function availableTokensForCurrentStage() public view returns (uint256) {
+   function availableTokensForCurrentStage() public view returns (uint256) {
         SaleStages _stage = currentSaleStage();
 
         uint256 maxTokens;
@@ -85,4 +71,5 @@ contract MyTokenCrowdsale is Crowdsale {
     function currentTokenPriceInCents() external view returns (uint256) {
         return pricePerTokenForStage[currentSaleStage()];
     }
+    */
 }
