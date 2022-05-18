a2a35e2eebfec65e7ccccb62e12d82207b6d1065
omkar <omkar.c.344@gmail.com>
2022-05-18 13:24:09 +0530

    changed version according to contract requirements

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,10 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.5.0",
+        version: "0.6.0",
       },
       {
-        version: "0.5.5",
+        version: "0.6.2",
       },
       {
         version: "0.8.0",
