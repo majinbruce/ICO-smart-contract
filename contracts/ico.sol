69ab323b982f54a10498a2d0a49249014df672ac
omkar <omkar.c.344@gmail.com>
2022-05-18 13:22:40 +0530

    made import changes and version

--- contracts/ico.sol
+++ contracts/ico.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "./crowdsale.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 
@@ -14,9 +14,12 @@ contract MyTokenCrowdsale is Crowdsale {
     uint256 preSaleQty = 30000000 * 10**9;
     uint256 seedSaleQty = 50000000 * 10**9;
     uint256 publicSaleQty = 20000000 * 10**9;
-    uint256 preSalePrice = 22666;
-    uint256 seedSalePrice = 13600;
-    uint256 publicSalePrice = 3400;
+
+    //
+
+    uint256 preSaleRate = 22666;
+    uint256 seedSaleRate = 13600;
+    uint256 publicSaleRate = 3400;
 
     // crowdsale stages
     uint256 preSale = 0;
@@ -26,7 +29,7 @@ contract MyTokenCrowdsale is Crowdsale {
     //this mapping tracs the amount of tokens available per stage
     mapping(uint256 => uint256) private stageTokens;
 
-    function setStageTokenMapping() private {
+    function setStageTokensMapping() private {
         stageTokens[preSale] = preSaleQty;
         stageTokens[seedSale] = seedSaleQty;
         stageTokens[publicSale] = publicSaleQty;
@@ -35,41 +38,51 @@ contract MyTokenCrowdsale is Crowdsale {
     //this mapping tracs the rate per stage
     mapping(uint256 => uint256) private stageRate;
 
-    // function getRate() private view returns () {}
+    function setStageRateMapping() private {
+        stageRate[preSale] = preSaleRate;
+        stageRate[seedSale] = seedSaleRate;
+        stageRate[publicSale] = publicSaleRate;
+    }
 
-    function _getTokenAmount(uint256 weiAmount, uint256 rate)
+    // function getRate() private view returns () {}
+    uint256 contractStage;
+
+    function _getTokenAmount(uint256 weiAmount)
         internal
         view
+        override
         returns (uint256)
     {
-        {
-            return (weiAmount.mul(rate));
+        if (contractStage == preSale) {
+            return (weiAmount.mul(preSaleRate));
+        } else if (contractStage == seedSale) {
+            return (weiAmount.mul(seedSaleRate));
+        } else {
+            return (weiAmount.mul(publicSaleRate));
         }
     }
-    /*
-    function currentSaleStage() public view returns (crowdSaleStage _stage) {
-        return saleStage;
-    }
 
-   function availableTokensForCurrentStage() public view returns (uint256) {
-        SaleStages _stage = currentSaleStage();
+    function _processPurchase(address beneficiary, uint256 tokenAmount)
+        internal
+        override
+    {
+        if (contractStage == 0) {
+            require(tokenAmount < stageTokens[preSale] && tokenAmount != 0);
+            stageTokens[preSale] = stageTokens[preSale].sub(tokenAmount);
 
-        uint256 maxTokens;
+            if (preSaleQty == 0) {
+                currentStage = 1;
+            }
+        } else if (currentStage == 1) {
+            seedSaleQty = seedSaleQty.sub(tokenAmount);
 
-        if (_stage == SaleStages.PreSale) {
-            maxTokens = tokensAvailableForStage[SaleStages.PreSale];
-        } else if (_stage == SaleStages.SeedSale) {
-            maxTokens = (tokensAvailableForStage[SaleStages.PreSale] +
-                tokensAvailableForStage[SaleStages.SeedSale]);
-        } else if (_stage == SaleStages.FinalSale) {
-            maxTokens = totalTokensToSell;
+            if (seedSaleQty == 0) {
+                currentStage = 2;
+            }
+        } else if (currentStage == 2) {
+            publicSaleQty = publicSaleQty.sub(tokenAmount);
         }
 
-        return maxTokens - tokensSold;
+        super._processPurchase(beneficiary, tokenAmount);
     }
-
-    function currentTokenPriceInCents() external view returns (uint256) {
-        return pricePerTokenForStage[currentSaleStage()];
-    }
-    */
 }
