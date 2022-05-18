43f10c344fa62fd89cb90121686978283a0847d8
omkar <omkar.c.344@gmail.com>
2022-05-18 13:22:56 +0530

    made import changes and version

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLISENCED
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
     constructor(uint256 totalSupply) public ERC20("MyToken", "MTN") {
