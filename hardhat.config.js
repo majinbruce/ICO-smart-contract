b978f2f20fe2144990cbc0037060995a950bc1cc
omkar <omkar.c.344@gmail.com>
2022-05-20 19:35:08 +0530

    added etherscan api key, testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,25 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("@nomiclabs/hardhat-waffle");
-//require("@openzeppelin/contracts");
+
+const { RINKEBY_PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
   solidity: {
     compilers: [
@@ -12,9 +29,17 @@ module.exports = {
       {
         version: "0.6.2",
       },
-      {
-        version: "0.8.0",
-      },
     ],
+  },
+
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    },
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
